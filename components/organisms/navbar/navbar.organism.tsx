5925a6da730fd3b356fa0020e2a16edeb0fc908d
NjohPrince <jufredprince@gmail.com>
2025-10-11 06:23:49 +0100

    fix(hero): worked on the responsiveness updates

--- components/organisms/navbar/navbar.organism.tsx
+++ components/organisms/navbar/navbar.organism.tsx
@@ -2,13 +2,15 @@
 
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
-import gbl from "../../../app/globals.module.css";
 import cls from "./navbar.module.css";
+import gbl from "../../../app/globals.module.css";
 
 import MenuLinksMolecule from "@/components/molecules/menu-links/menu-links.molecule";
 import CloseIcon from "@/components/icons/close.icon";
 import BurgerIcon from "@/components/icons/burger.icon";
+import BookIcon from "@/components/icons/book.icon";
 
 const NavbarOrganism = () => {
   const [isActive, setIsActive] = React.useState(false);
@@ -19,13 +21,21 @@ const NavbarOrganism = () => {
       aria-label="main navigation"
       className={`${cls.navbar} ${gbl["py-24"]} ${gbl.px} ${gbl.flex} ${gbl["gap-24"]} ${gbl["justify-between"]} ${gbl["items-center"]}`}
     >
+      <button
+        className={`${cls.book__btn}`}
+        aria-label="book appointment button"
+      >
+        <BookIcon />
+      </button>
       <div className={`${cls.logo}`}>
-        <Image
-          width={150}
-          height={150}
-          alt="cedars medicare logo"
-          src={"/assets/images/cedars-logo.webp"}
-        />
+        <Link href={"/"} aria-label="home link">
+          <Image
+            width={150}
+            height={150}
+            alt="cedars medicare logo"
+            src={"/assets/images/cedars-logo.webp"}
+          />
+        </Link>
       </div>
       <div className={`${gbl.flex} ${gbl["gap-24"]} ${gbl["items-center"]}`}>
         <MenuLinksMolecule isActive={isActive} />
