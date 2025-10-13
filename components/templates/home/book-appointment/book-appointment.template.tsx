9943858bf7380b7040aa32cdf1064507cfd77403
NjohPrince <jufredprince@gmail.com>
2025-10-13 09:58:43 +0100

    feat(component): updated state

--- components/templates/home/book-appointment/book-appointment.template.tsx
+++ components/templates/home/book-appointment/book-appointment.template.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Slide, Fade } from "react-awesome-reveal";
 import Image from "next/image";
 
 import gbl from "../../../../app/globals.module.css";
 import cls from "./book-appointment.module.css";
+import ButtonAtom from "@/components/atoms/button/button.atom";
+import ForwardArrowIcon from "@/components/icons/forward-arrow.icon";
 
 const BookAppointmentTemplate = () => {
+  const [selectedDay, setSelectedDay] = useState("");
+  const days = ["Mon", "Tues", "Wed", "Thurs", "Fri"];
+
   return (
     <section
       id="book-appointment"
-      className={`${gbl["w-full"]} ${cls.book} ${gbl.px} ${gbl["py-48"]} ${gbl.flex} ${gbl["gap-32"]}`}
+      className={`${gbl["w-full"]} ${cls.book} ${gbl.px} ${gbl["py-48"]} ${gbl.flex} ${gbl["gap-32"]} ${gbl["items-center"]} ${gbl["justify-center"]}`}
     >
       <div className={`${cls.image} ${gbl.flex} ${gbl["items-center"]}`}>
         <Fade direction="left" triggerOnce>
@@ -33,6 +40,55 @@ const BookAppointmentTemplate = () => {
             better healthcare.
           </p>
         </Slide>
+
+        <form
+          className={`${cls.subscribe} ${gbl.flex} ${gbl["flex-col"]} ${gbl["gap-24"]}`}
+        >
+          <div
+            className={`${cls.day__select} ${gbl.flex} ${gbl["gap-8"]} ${gbl["flex-col"]}`}
+          >
+            <h3>Select Preferred Days</h3>
+            <div className={`${cls.days} ${gbl.flex} ${gbl["gap-8"]}`}>
+              {days &&
+                days.length > 0 &&
+                days.map((day, index) => {
+                  return (
+                    <button
+                      key={index}
+                      onClick={() => {
+                        setSelectedDay(day);
+                      }}
+                      className={`${selectedDay === day ? cls.active : ""} ${gbl['t-delay-3']}`}
+                    >
+                      {day}
+                    </button>
+                  );
+                })}
+            </div>
+          </div>
+          <div
+            className={`${cls.details} ${gbl.flex} ${gbl["flex-col"]} ${gbl["gap-8"]}`}
+          >
+            <h3>Enter Your Details</h3>
+            <div className={`${gbl.flex} ${gbl["gap-8"]}`}>
+              <input type="text" placeholder="First Name" />
+              <input type="text" placeholder="Last Name" />
+            </div>
+            <div className={`${gbl.flex} ${gbl["gap-8"]}`}>
+              <input type="text" placeholder="E-Mail Address" />
+              <input type="text" placeholder="Phone Number" />
+            </div>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Message"
+            ></textarea>
+          </div>
+          <ButtonAtom
+            btnText="Submit Request"
+            iconRight={<ForwardArrowIcon size="20" />}
+          />
+        </form>
       </div>
     </section>
   );
